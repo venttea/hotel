14d68d1055c5f6662034a80fe39acaed15b23ede
venttea <keshasolnshko@gmail.com>
2025-12-17 19:22:55 +0700

    Удаление пути до страницы "Просмотр записей клиентов на услугу"

--- hotel_business/hotel_business/urls.py
+++ hotel_business/hotel_business/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from hotel import views as hotel_views
+from hotel import views
 
 urlpatterns = [
 
@@ -8,34 +8,32 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Вход в систему
-    path('', hotel_views.login_view, name='login'),
-    path('login/', hotel_views.login_view, name='login'),
-    path('logout/', hotel_views.logout_view, name='logout'),
+    path('', views.login_view, name='login'),
 
-    # Главная страница (после входа)
-    path('home/', hotel_views.home, name='home'),
+    # Вход в систему
+    path('login/', views.login_view, name='login'),
 
-    # Страница доступная без авторизации
-    path('services/', hotel_views.service_list, name='service_list'),
+    # Выход из системы
+    path('logout/', views.logout_view, name='logout'),
+
+    # Главная страница (после авторизации)
+    path('home/', views.home, name='home'),
+
+    # Список услуг доступный всем
+    path('services/', views.service_list, name='service_list'),
 
     # Страница имеющихся бронирований
-    path('bookings/', hotel_views.booking_list, name='booking_list'),
+    path('bookings/', views.booking_list, name='booking_list'),
 
     # Страница создания бронирования
-    path('bookings/create/', hotel_views.booking_create, name='booking_create'),
+    path('bookings/create/', views.booking_create, name='booking_create'),
 
-    # Просмотр списка клиентов от лица менеджера
-    path('manager/clients/', hotel_views.client_list, name='client_list'),
+    # Просмотр списка клиентов для менеджера
+    path('manager/clients/', views.client_list, name='client_list'),
 
-    # Просмотр услуг от лмца менеджера
-    path('manager/services/', hotel_views.manager_service_list, name='manager_service_list'),
+    # Страница номерного фонда с фильтрацией
+    path('manager/rooms/', views.room_list, name='room_list'),
 
-    # Страница номерного фонда
-    path('manager/rooms/', hotel_views.room_list, name='room_list'),
-
-    # Страница записи клиента на услугу
-    path('manager/book-service/', hotel_views.book_service, name='book_service'),
-
-    # Страница просмотра записей клиентов на услуги
-    path('manager/service-bookings/', hotel_views.view_service_bookings, name='view_service_bookings'),
+    # Страница записи клиента на услугу для менеджера
+    path('manager/book-service/', views.book_service, name='book_service'),
 ]
